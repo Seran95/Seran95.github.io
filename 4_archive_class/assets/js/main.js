c1f4a48f44462ce3542ff9425fdb214fb19122e3
Seran <shar0018@student.monash.edu>
2018-05-25 15:49:21 +1000

    update

---
+++ 4_archive_class/assets/js/main.js
@@ -0,0 +1,113 @@
+$(document).ready(function() {
+// Add jQuery here
+
+
+$('.b-bridges').click(function(){
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.bridges').removeClass('hide');
+  $('.item.bridges').addClass('show');
+});
+
+$('.b-castles').click(function(){
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.castles').removeClass('hide');
+  $('.item.castles').addClass('show');
+});
+
+
+$('.b-dome').click(function(){
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.dome').removeClass('hide');
+  $('.item.dome').addClass('show');
+});
+
+$('.b-green').click(function(){
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.green').removeClass('hide');
+  $('.item.green').addClass('show');
+});
+
+$('.b-modern').click(function(){
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.modern').removeClass('hide');
+  $('.item.modern').addClass('show');
+});
+
+$('.b-mountains').click(function(){
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.mountains').removeClass('hide');
+  $('.item.mountains').addClass('show');
+});
+
+$('.b-people').click(function(){
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.people').removeClass('hide');
+  $('.item.people').addClass('show');
+});
+
+$('.b-pillars').click(function(){
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.pillars').removeClass('hide');
+  $('.item.pillars').addClass('show');
+});
+
+$('.b-pointy').click(function(){
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.pointy').removeClass('hide');
+  $('.item.pointy').addClass('show');
+});
+
+$('.b-rocks').click(function(){
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.rocks').removeClass('hide');
+  $('.item.rocks').addClass('show');
+});
+
+$('.b-water').click(function(){
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.water').removeClass('hide');
+  $('.item.water').addClass('show');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Don't remove this line
+  });
