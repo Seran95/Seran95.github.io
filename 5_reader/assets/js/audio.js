41789b48d408ae91283cee1bce513babebde672c
Seran <shar0018@student.monash.edu>
2018-06-01 17:32:23 +1000

    push

--- 5_reader/assets/js/audio.js
+++ 5_reader/assets/js/audio.js
@@ -1,32 +1,43 @@
 $(document).ready(function() {
 
-  var audioBell = document.createElement('audio');
-  audioBell.setAttribute('src', 'h2.mp3');
+  var audioHomer = document.createElement('audio');
+  audioHomer.setAttribute('src', 'https://rawgit.com/Seran95/Seran95.github.io/master/5_reader/assets/audio/h2.mp3');
   $.get();
-  audioBell.addEventListener("load", function() {
-    audioBell.play();
+  audioHomer.addEventListener("load", function() {
+    audioHomer.play();
   }, true);
 
-  var audioHorn = document.createElement('audio');
-  audioHorn.setAttribute('src', 'https://www.soundjay.com/misc/hohner-melodica-1.mp3');
+  var audioFILENAME2 = document.createElement('audio');
+  audioFILENAME2.setAttribute('src', 'https://rawgit.com/Seran95/Seran95.github.io/master/5_reader/assets/audio/h2.mp3');
   $.get();
-  audioHorn.addEventListener("load", function() {
-    audioHorn.play();
+  audioFILENAME2.addEventListener("load", function() {
+    audioFILENAME2.play();
   }, true);
+
+  var audioFilename3 = document.createElement('audio');
+  audioFilename3.setAttribute('src', 'https://rawgit.com/Seran95/Seran95.github.io/master/5_reader/assets/audio/h2.mp3');
+  $.get();
+  audioFilename3.addEventListener("load", function() {
+    audioFilename3.play();
+  }, true);
+
+
 
 
 
 
 
   $('.play_homer').click(function() {
-    audioBell.play();
+    audioHomer.play();
   });
 
-
-  $('.pause_bell').click(function() {
-    audioBell.pause();
+  $('.play_homer2222').click(function() {
+    audioFILENAME2.play();
   });
 
+  $('.play_homer333').click(function() {
+    audioFILENAME3.play();
+  });
 
 
 
